2c75604cf0751f9dba74d8c35e57b338d6f2a067
Sébastien Denooz <sebastien@clicandwalk.com>
2014-09-13 11:47:44 +0200

    Le background qui fonctionne

--- www/js/index.js
+++ www/js/index.js
@@ -1,14 +1,24 @@
 var app = {
-    initialize: function() {
-        this.bindEvents();
-    },
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    onDeviceReady: function() {
-        navigation.init();
-    },
-    receivedEvent: function(id) {
-        
-    }
+	initialize: function() {
+		this.bindEvents();
+	},
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	onDeviceReady: function() {
+		navigation.init();
+		var timerCount = 0;
+		window.plugins.BackgroundJS.LockBackgroundTime(
+			function(){}, 
+			function(msg){
+				console.log(msg);
+			}
+		);
+		setInterval(function() {
+			$('body').html(timerCount++);
+		},100);
+	},
+	receivedEvent: function(id) {
+
+	}
 };
